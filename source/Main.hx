f38e89e65536fe62e1312f7151ad5b7929ee3372
CeliaYZhang <czhang2@caltech.edu>
2015-09-20 03:10:36 -0700

    Changed starting state from MainState to LoginState.

--- source/Main.hx
+++ source/Main.hx
@@ -7,13 +7,13 @@ import flash.events.Event;
 import flash.Lib;
 import flixel.FlxGame;
 import flixel.FlxState;
-import states.MainState;
+import states.LoginState;
 
 class Main extends Sprite 
 {
 	var gameWidth:Int = 640; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 480; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = MainState; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = LoginState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
