e0b75555fea573769c1cc50e5ee8b0de0e421ded
Loko Kung <lokokung@gmail.com>
2015-09-20 10:38:16 -0700

    Added ability to change fonts for the InputText.

--- source/AssetPaths.hx
+++ source/AssetPaths.hx
@@ -1,7 +1,8 @@
 package;
 
-@:build(flixel.system.FlxAssets.buildFileReferences("assets/images", false))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/pokesprites", false))
 @:build(flixel.system.FlxAssets.buildFileReferences("assets/data", false))
 @:build(flixel.system.FlxAssets.buildFileReferences("assets/tiles", false))
 @:build(flixel.system.FlxAssets.buildFileReferences("assets/levels", false))
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/fonts", false))
 class AssetPaths {}
