888fed2bd973034d1888f897936913d580d5ad21
Loko Kung <lokokung@gmail.com>
2015-09-25 17:14:54 -0700

    Login to server/db implemented.

---
+++ source/connection/LoginData.hx
@@ -0,0 +1,18 @@
+package connection;
+
+/**
+ * ...
+ * @author Lokbondo Kung; Celia Zhang
+ */
+class LoginData
+{
+	private var username:String;
+	private var password:String;
+	
+	public function new(username:String, password:String) 
+	{
+		this.username = username;
+		this.password = password;
+	}
+	
+}
--- source/connection/RequestHandler.hx
+++ source/connection/RequestHandler.hx
@@ -4,6 +4,7 @@ import sys.io.File;
 import sys.net.Host;
 import sys.net.Socket;
 import haxe.Json;
+import haxe.crypto.Md5;
 
 /**
  * ...
@@ -23,27 +24,60 @@ class RequestHandler
 		
 		hostName = File.getContent(AssetPaths.ServerHost__txt);
 		port = Std.parseInt(File.getContent(AssetPaths.ServerPort__txt));
+		tcpSocket = null;
 	}
 	
 	public function requestSession()
 	{
 		sessionId = null;
+		if (tcpSocket != null)
+		{
+			tcpSocket.close();
+			tcpSocket = null;
+		}
 		
 		requestPacket = new RequestPacket(
 			RequestCode.Session, null, sessionId);
 		var jsonPacket = Json.stringify(requestPacket);
 		
 		tcpSocket = new Socket();
-		tcpSocket.setTimeout(3);
+		tcpSocket.setTimeout(10);
 		tcpSocket.connect(new Host(hostName), port);
 		
 		tcpSocket.output.writeString(jsonPacket + "\n");
 		tcpSocket.output.flush();
 		
 		sessionId = tcpSocket.input.readLine();
+		trace(sessionId);
+	}
+	
+	public function requestLogin(Username:String, Password:String):Bool
+	{
+		if (sessionId == null || tcpSocket == null)
+			return false;
+			
+		// Encode password.
+		var firstEncode = Md5.encode(Password);
+		var secondEncode = sessionId + firstEncode + sessionId;
+		var encodedPass = Md5.encode(secondEncode);
 		
-		tcpSocket.close();
-		tcpSocket = null;
+		// Get json for LoginInfo
+		var loginInfo = new LoginData(Username, encodedPass);
+		var loginPacket = Json.stringify(loginInfo);
+		
+		requestPacket = new RequestPacket(
+			RequestCode.Login, loginPacket, sessionId);
+		var jsonPacket = Json.stringify(requestPacket);
+		
+		tcpSocket.output.writeString(jsonPacket + "\n");
+		tcpSocket.output.flush();
+		
+		tcpSocket.input.readLine();
+		var serverReply = tcpSocket.input.readLine();
+		trace(serverReply);
+		if (serverReply == "true")
+			return true;
+		return false;
 	}
 	
 }
